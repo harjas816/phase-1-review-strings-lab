6af1eaee352c5173e611592a046515e9547b5eea
harjas816 <harjas816@gmail.com>
2023-05-22 11:21:47 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser="Bill"
+let welcomeMessage = "Welcome to Flatbook, Bill !"
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, BILL!"
+let shortGreeting = "Welcome, " + currentUser[0] + "!"
